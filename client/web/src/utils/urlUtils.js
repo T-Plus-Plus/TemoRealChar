fc7c8eff7bc79ce6c27c524fdb5b52824282e94d
Bram Desoete <10937924+bramdes@users.noreply.github.com>
2023-08-28 08:58:58 +0800

    getting it to start on ec2

--- client/web/src/utils/urlUtils.js
+++ client/web/src/utils/urlUtils.js
@@ -6,6 +6,7 @@
  */
 import { isIP } from 'is-ip';
 
+// there is a bug in this block, it does not work on ec2 if its hosted on another port like 3000, it just goes to the default port 80
 export const getHostName = () => {
   var currentHost = window.location.host;
   var parts = currentHost.split(':');
@@ -16,8 +17,8 @@ export const getHostName = () => {
   if (!(hostname === 'localhost' || isIP(hostname))) {
     // Remove www. from hostname
     hostname = hostname.replace('www.', '');
-    hostname = 'api.' + hostname;
-    newPort = window.location.protocol === 'https:' ? 443 : 80;
+    //hostname = 'api.' + hostname;
+    newPort = window.location.protocol === 'https:' ? 443 : 8000;
   }
   var newHost = hostname + ':' + newPort;
   return newHost;
